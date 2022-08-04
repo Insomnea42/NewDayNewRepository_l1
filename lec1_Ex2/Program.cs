8fe7e96df912969eb08f8deaf58a70bf45fc7655
Insomnea42 <khveroe@gmail.com>
2022-08-04 16:34:35 +0500

    Добавлены папка Привет юзер, плюс гитигнор

---
+++ lec1_Ex2/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите ваше имя!");
+string UserName =  Console.ReadLine();
+Console.WriteLine ("Привет, ");
+Console.WriteLine(UserName);
+
