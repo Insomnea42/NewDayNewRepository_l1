7fbd934ac38a1852671ba02b0830b18d41a4b268
Insomnea42 <khveroe@gmail.com>
2022-08-04 16:57:16 +0500

    Дополнен Приер 3 new Random().Next(1, 10); // рандомное Целое число в диапозоне 1..9

--- lec1_Ex3/Program.cs
+++ lec1_Ex3/Program.cs
@@ -1,4 +1,5 @@
-﻿int NumberA = 31;
+﻿int NumberA = new Random().Next(1, 10); // рандомное Целое число в диапозоне 1..9
+Console.WriteLine (NumberA);
 int NumberB = -6;
 int result = NumberA + NumberB;
 Console.WriteLine(result);
--- lec1_Ex4/Program.cs
+++ lec1_Ex4/Program.cs
@@ -1,3 +1,3 @@
-﻿double numA = 14; // вещественное число
+﻿double numA = 0; // вещественное число
 int numB = 5; // целое число
 Console.WriteLine(numA/numB);
