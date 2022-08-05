78cd6b6d2d5fa22e1d5e7f462338563893f2101b
Insomnea42 <khveroe@gmail.com>
2022-08-05 09:04:24 +0500

    adding Ex5 first if else

---
+++ lec1_Ex5/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write ("Введите имя пользователя ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "ника")
+    {
+        Console.WriteLine("Ура, Это Ты!!");
+    }
+else
+    {
+        Console.Write ("Привет, "+username);
+    }
